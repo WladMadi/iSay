c3b6ac7367c8250aa697679adca73acf1918695c
Влад Мади <wladmadi@icloud.com>
2017-08-03 18:53:42 +0500

    Initial Commit

---
+++ iSay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSay
+//
+//  Created by Влад Мади on 03.08.2017.
+//  Copyright © 2017 ВладМади. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
